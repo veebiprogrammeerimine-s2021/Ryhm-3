601abe60277dd5a0a2e7e0b93893eb3d970de1d0
Andrus Rinde <andrusrinde@users.noreply.github.com>
2021-11-10 12:30:27 +0200

    Üheksas kodutöö tehtud

---
+++ 10_tund/fnc_movie.php
@@ -0,0 +1,144 @@
+<?php
+    //$database = "if21_inga_pe_T1";
+    $database = "if21_inga_pe_E1";
+    
+    function read_all_person_for_select($selected){
+        $options_html = null;
+        $conn = new mysqli($GLOBALS["server_host"], $GLOBALS["server_user_name"], $GLOBALS["server_password"], $GLOBALS["database"]);
+        $conn->set_charset("utf8");
+        $stmt = $conn->prepare("SELECT * FROM person");
+        //<option value="x" selected>Eesnimi Perekonnanimi (sünnipäev)</option>
+        echo $conn->error;
+        $stmt->bind_result($id_from_db, $first_name_from_db, $last_name_from_db, $birth_date_from_db);
+        $stmt->execute();
+        while($stmt->fetch()){
+            $options_html .= '<option value="' .$id_from_db .'"';
+            if($id_from_db == $selected){
+                $options_html .= " selected";
+            }
+            $options_html .= ">" .$first_name_from_db ." " .$last_name_from_db ." (" .$birth_date_from_db .")</options> \n";
+        }
+        $stmt->close();
+        $conn->close();
+        return $options_html;
+    }
+    
+    function read_all_movie_for_select($selected){
+        $options_html = null;
+        $conn = new mysqli($GLOBALS["server_host"], $GLOBALS["server_user_name"], $GLOBALS["server_password"], $GLOBALS["database"]);
+        $conn->set_charset("utf8");
+        $stmt = $conn->prepare("SELECT id, title, production_year FROM movie");
+        echo $conn->error;
+        $stmt->bind_result($id_from_db, $title_from_db, $production_year_from_db);
+        $stmt->execute();
+        while($stmt->fetch()){
+            $options_html .= '<option value="' .$id_from_db .'"';
+            if($id_from_db == $selected){
+                $options_html .= " selected";
+            }
+            $options_html .= ">" .$title_from_db ." (" .$production_year_from_db .")</options> \n";
+        }
+        
+        $stmt->close();
+        $conn->close();
+        return $options_html;
+    }
+    
+    function read_all_position_for_select($selected){
+        $options_html = null;
+        $conn = new mysqli($GLOBALS["server_host"], $GLOBALS["server_user_name"], $GLOBALS["server_password"], $GLOBALS["database"]);
+        $conn->set_charset("utf8");
+        $stmt = $conn->prepare("SELECT id, position_name FROM position");
+        echo $conn->error;
+        $stmt->bind_result($id_from_db, $position_name_from_db);
+        $stmt->execute();
+        while($stmt->fetch()){
+            $options_html .= '<option value="' .$id_from_db .'"';
+            if($id_from_db == $selected){
+                $options_html .= " selected";
+            }
+            $options_html .= ">" .$position_name_from_db ."</options> \n";
+        }
+        
+        $stmt->close();
+        $conn->close();
+        return $options_html;
+    }
+    
+    function store_person_movie_relation($selected_person, $selected_movie, $selected_position, $role){
+        $notice = null;
+        $conn = new mysqli($GLOBALS["server_host"], $GLOBALS["server_user_name"], $GLOBALS["server_password"], $GLOBALS["database"]);
+        $conn->set_charset("utf8");
+        $stmt = $conn->prepare("SELECT id, role FROM person_in_movie WHERE person_id = ? AND movie_id = ? AND position_id = ?");
+        echo $conn->error;
+        $stmt->bind_param("iii", $selected_person, $selected_movie, $selected_position);
+        $stmt->bind_result($id_from_db, $role_from_db);
+        $stmt->execute();
+        if($stmt->fetch()){
+            if($role_from_db == $role){
+                //selline on olemas
+                $notice = "Selline seos on juba olemas!";
+            }
+        }
+        if(empty($notice)){
+            $stmt->close();
+            if($selected_person == 1){
+                $role = "";
+            }
+            $stmt = $conn->prepare("INSERT INTO person_in_movie (person_id, movie_id, position_id, role) VALUES (?, ?, ?, ?)"); 
+            $stmt->bind_param("iiis", $selected_person, $selected_movie, $selected_position, $role);
+            if($stmt->execute()){
+                $notice = "Uus seos edukalt salvestatud!";
+            } else {
+                $notice = "Uue seose salvestamisel tekkis viga: " .$stmt->error;
+            }
+        }
+        $stmt->close();
+        $conn->close();
+        return $notice;
+    }
+
+    function store_person_photo($file_name, $person_id){
+        $notice = null;
+        $conn = new mysqli($GLOBALS["server_host"], $GLOBALS["server_user_name"], $GLOBALS["server_password"], $GLOBALS["database"]);
+        $conn->set_charset("utf8");
+        $stmt = $conn->prepare("INSERT INTO picture (picture_file_name, person_id) VALUES (?, ?)");
+        echo $conn->error;
+        $stmt->bind_param("si", $file_name, $person_id);
+        if($stmt->execute()){
+            $notice = "Uus foto edukalt salvestatud!";
+        } else {
+            $notice = "Uue foto andmebaasi salvestamisel tekkis viga: " .$stmt->error;
+        }
+        $stmt->close();
+        $conn->close();
+        return $notice;
+    }
+    
+    
+    function list_person_movie_info(){
+		$html = null;
+        $conn = new mysqli($GLOBALS["server_host"], $GLOBALS["server_user_name"], $GLOBALS["server_password"], $GLOBALS["database"]);
+        $conn->set_charset("utf8");
+		$stmt = $conn->prepare("SELECT person.first_name, person.last_name, person.birth_date, person_in_movie.role, movie.title, movie.production_year, movie.duration FROM person JOIN person_in_movie ON person.id = person_in_movie.person_id JOIN movie ON movie.id = person_in_movie.movie_id");
+		echo $conn->error;
+		$stmt->bind_result($first_name_from_db, $last_name_from_db, $birth_date_from_db, $role_from_db, $title_from_db, $production_year_from_db, $duration_from_db);
+		$stmt->execute();
+		while($stmt->fetch()){
+			$html .= "<li>" .$first_name_from_db ." " .$last_name_from_db ." (sündinud: " .date_to_est_format($birth_date_from_db) ."), ";
+			if(!empty($role_from_db)){
+				$html .= "tegelane " .$role_from_db .' filmis "' .$title_from_db .'" (toodetud: ' .$production_year_from_db .", kestus: " .duration_min_to_hour_and_min($duration_from_db) .")";
+			} else {
+				$html .= '"' .$title_from_db .'" (toodetud: ' .$production_year_from_db .", kestus: " .duration_min_to_hour_and_min($duration_from_db) .")";
+			}
+			$html .= "</li> \n";
+		}
+		if(empty($html)){
+			$html = "<p>Info puudub.</p> \n";
+		} else {
+            $html = "<ul> \n" .$html ."</ul> \n";
+        }
+		$stmt->close();
+        $conn->close();
+        return $html;
+	}
---
+++ 10_tund/gallery_own.php
@@ -0,0 +1,67 @@
+<?php
+    //alustame sessiooni
+    session_start();
+    //kas on sisselogitud
+    if(!isset($_SESSION["user_id"])){
+        header("Location: page.php");
+    }
+    //väljalogimine
+    if(isset($_GET["logout"])){
+        session_destroy();
+        header("Location: page.php");
+    }
+	
+    require_once("../../../../config_vp_s2021.php");
+    require_once("fnc_general.php");
+    require_once("fnc_gallery.php");
+    $privacy = 2;
+    
+    //greeny.cs.tlu.ee/~rinde/vp2021/Ryhm-3/09_tund/gallery_public.php?page=2
+    $page = 1;
+    $limit = 10;
+    $photo_count = count_own_photos();
+    //kontrollime, mis lehel oleme ja kas selline leht on võimalik
+    if(!isset($_GET["page"]) or $_GET["page"] < 1){
+        $page = 1;
+    } elseif(round($_GET["page"] - 1) * $limit >= $photo_count){
+        $page = ceil($photo_count / $limit);
+    } else {
+        $page = $_GET["page"];
+    }
+    
+    $to_head = '<link rel="stylesheet" type="text/css" href="style/gallery.css">' . "\n";
+    require("page_header.php");
+?>
+
+	<h1><?php echo $_SESSION["first_name"] ." " .$_SESSION["last_name"]; ?>, veebiprogrammeerimine</h1>
+	<p>See leht on valminud õppetöö raames ja ei sisalda mingisugust tõsiseltvõetavat sisu!</p>
+	<p>Õppetöö toimus <a href="https://www.tlu.ee/dt">Tallinna Ülikooli Digitehnoloogiate instituudis</a>.</p>
+	<hr>
+    <ul>
+        <li><a href="?logout=1">Logi välja</a></li>
+		<li><a href="home.php">Avaleht</a></li>
+    </ul>
+	<hr>
+    <h2>Avalike fotode galerii</h2>
+    <div class="gallery">
+        <p>
+            <?php
+                //Eelmine leht | Järgmine leht
+                //<span>Eelmine leht</span> | <span><a href="?page=2">Järgmine leht</a></span>
+                if($page > 1){
+                    echo '<span><a href="?page=' .($page - 1) .'">Eelmine leht</a></span>';
+                } else {
+                    echo "<span>Eelmine leht</span>";
+                }
+                echo " | ";
+                if($page * $limit < $photo_count){
+                    echo '<span><a href="?page=' .($page + 1) .'">Järgmine leht</a></span>';
+                } else {
+                    echo "<span>Järgmine leht</span>";
+                }
+            ?>
+        </p>
+        <?php echo read_own_photo_thumbs($page, $limit); ?>
+    </div>
+</body>
+</html>
